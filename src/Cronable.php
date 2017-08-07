b43a6c1bb1a88304a1954c6e4c0855fd4ebdc8b0
Philipp Lang <pille@zoomyboy.de>
2017-08-07 02:09:41 +0200

    Fixed: Ensure that cron models are deleted when cronable is deleted Otherwise you could end up calling the runCron() function on null values when the schedule runs...

--- src/Cronable.php
+++ src/Cronable.php
@@ -6,4 +6,10 @@ trait Cronable {
 	public function cron() {
 		return $this->morphOne(\Zoomyboy\Cronable\Cron::class, 'cron');
 	}
+
+	public static function bootCronable() {
+		static::deleting(function($model) {
+			$model->cron->delete();
+		});
+	}
 }
